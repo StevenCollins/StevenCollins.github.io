43aee04d06aa588e888d7e6cddf45256ab063957
Steven Collins <StevenCollins@users.noreply.github.com>
2017-08-22 22:15:22 -0400

    Fixed blasphemy (converted tabs to spaces)

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ var commands = {}
 var state = {}
 
 commands.help = function() {
-	return 'I\'m working on it!';
+    return 'I\'m working on it!';
 }
 
 commands.echo = function (args) {
-	args.shift();
-	return args.join(' ');
+    args.shift();
+    return args.join(' ');
 }
 
 commands.su = function (args) {
-	if (args.length > 1) {
-		Terminal.user = args[1];
-	}
-	return '';
+    if (args.length > 1) {
+        Terminal.user = args[1];
+    }
+    return '';
 }
 
 commands.exit = function (args) {
-	Terminal.exit();
-	console.log('[Process completed]');
+    Terminal.exit();
+    console.log('[Process completed]');
 }
 
 Terminal.init('screen', {
-	commands: commands,
-	prompt: '\\u@\\H $ ',
-	intro: "<p>Welcome to Steven Collins' website.<br>Please take a look around, but there's not much to see yet.</p>"
+    commands: commands,
+    prompt: '\\u@\\H $ ',
+    intro: "<p>Welcome to Steven Collins' website.<br>Please take a look around, but there's not much to see yet.</p>"
 });
